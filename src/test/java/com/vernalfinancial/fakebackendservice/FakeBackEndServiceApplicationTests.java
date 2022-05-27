495f175bfbba92931c2c7e98150ccfa82a30768e
Matthew Crowell <>
2022-05-26 21:00:14 -0500

    add test class for application

---
+++ src/test/java/com/vernalfinancial/fakebackendservice/FakeBackEndServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vernalfinancial.fakebackendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeBackEndServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
