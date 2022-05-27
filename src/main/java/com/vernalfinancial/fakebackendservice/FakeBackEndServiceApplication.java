33c5f76de15381d57ddbb6d8ca00a63b3fb268ab
Matthew Crowell <>
2022-05-26 20:59:58 -0500

    update application file

---
+++ src/main/java/com/vernalfinancial/fakebackendservice/FakeBackEndServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vernalfinancial.fakebackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeBackEndServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeBackEndServiceApplication.class, args);
+	}
+
+}
