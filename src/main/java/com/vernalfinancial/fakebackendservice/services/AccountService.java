d689a2c43844c7c1ea3622a9ba363ee1eb05c226
Cahlien <78377986+Cahlien@users.noreply.github.com>
2022-06-03 07:46:15 -0500

    add get accounts method to accounts service

--- src/main/java/com/vernalfinancial/fakebackendservice/services/AccountService.java
+++ src/main/java/com/vernalfinancial/fakebackendservice/services/AccountService.java
@@ -1,7 +1,10 @@
 package com.vernalfinancial.fakebackendservice.services;
 
+import com.vernalfinancial.fakebackendservice.entities.VFFinancialAsset;
+
 import java.util.List;
 
 public interface AccountService {
+	List<VFFinancialAsset> getAccounts();
 //	List<VFAccount> getAccounts();
 }
--- src/main/java/com/vernalfinancial/fakebackendservice/services/AccountServiceImpl.java
+++ src/main/java/com/vernalfinancial/fakebackendservice/services/AccountServiceImpl.java
@@ -1,5 +1,8 @@
 package com.vernalfinancial.fakebackendservice.services;
 
+import com.vernalfinancial.fakebackendservice.entities.VFCheckingAccount;
+import com.vernalfinancial.fakebackendservice.entities.VFFinancialAsset;
+import com.vernalfinancial.fakebackendservice.entities.VFSavingsAccount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,21 +12,12 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
-//
-//	private final List<VFSavingsAccountModel> savingsAccounts;
-//	private final List<VFAccount> accountList;
-//
-//	public AccountServiceImpl() {
-//		this(new ArrayList<>(), new ArrayList<>());
-//
-//		for(int i = 0; i < 100; i++)
-//		{
-//			this.accountList.add(new VFSavingsAccountModel());
-//		}
-//	}
-//
-//	@Override
-//	public List<VFAccount> getAccounts() {
-//		return this.accountList;
-//	}
+	@Override
+	public List<VFFinancialAsset> getAccounts() {
+		List<VFFinancialAsset> accounts = new ArrayList<>();
+		accounts.add(new VFCheckingAccount());
+		accounts.add(new VFSavingsAccount());
+
+		return accounts;
+	}
 }
