e214a311a6c1925875d97b8c5c090645df2e8199
Matthew Crowell <78377986+Cahlien@users.noreply.github.com>
2022-06-03 20:05:29 -0500

    add login credentials

--- src/main/java/com/vernalfinancial/fakebackendservice/entities/VFIdentity.java
+++ src/main/java/com/vernalfinancial/fakebackendservice/entities/VFIdentity.java
@@ -1,8 +1,11 @@
 package com.vernalfinancial.fakebackendservice.entities;
 
 import com.vernalfinancial.fakebackendservice.models.VFRecordType;
+import org.apache.tomcat.jni.Local;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
@@ -25,6 +28,10 @@ public abstract class VFIdentity {
 	private List<VFOrganization> associatedOrganizations;
 	@ManyToMany
 	private List<VFPerson> associatedPeople;
+	@NotNull
+	private LocalDateTime createdAt;
+	@NotNull
+	private LocalDateTime modifiedAt;
 
 	/**
 	 * The default constructor for the VFIdentity class
@@ -41,12 +48,14 @@ public abstract class VFIdentity {
 	 * called by any other constructor in the class.
 	 *
 	 * @param associatedOrganizations List<VFOrganization> the list of associated organizations
-	 * @param associatedPeople List<VFPerson> the list of associated people
+	 * @param associatedPeople        List<VFPerson> the list of associated people
 	 */
 	public VFIdentity(List<VFOrganization> associatedOrganizations, List<VFPerson> associatedPeople) {
 		this.recordType = VFRecordType.Identity;
 		this.associatedOrganizations = associatedOrganizations;
 		this.associatedPeople = associatedPeople;
+		this.createdAt = LocalDateTime.now();
+		this.modifiedAt = LocalDateTime.now();
 	}
 
 	public String getId() {
@@ -55,6 +64,14 @@ public abstract class VFIdentity {
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public VFRecordType getRecordType() {
+		return recordType;
+	}
+
+	public void setRecordType(VFRecordType recordType) {
+		this.recordType = recordType;
 	}
 
 	public List<VFOrganization> getAssociatedOrganizations() {
@@ -73,25 +90,44 @@ public abstract class VFIdentity {
 		this.associatedPeople = associatedPeople;
 	}
 
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getModifiedAt() {
+		return modifiedAt;
+	}
+
+	public void setModifiedAt(LocalDateTime modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		VFIdentity that = (VFIdentity) o;
-		return Objects.equals(getId(), that.getId()) && Objects.equals(getAssociatedOrganizations(), that.getAssociatedOrganizations()) && Objects.equals(getAssociatedPeople(), that.getAssociatedPeople());
+		return Objects.equals(getId(), that.getId()) && getRecordType() == that.getRecordType() && Objects.equals(getAssociatedOrganizations(), that.getAssociatedOrganizations()) && Objects.equals(getAssociatedPeople(), that.getAssociatedPeople()) && Objects.equals(getCreatedAt(), that.getCreatedAt()) && Objects.equals(getModifiedAt(), that.getModifiedAt());
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(getId(), getAssociatedOrganizations(), getAssociatedPeople());
+		return Objects.hash(getId(), getRecordType(), getAssociatedOrganizations(), getAssociatedPeople(), getCreatedAt(), getModifiedAt());
 	}
 
 	@Override
 	public String toString() {
 		return "identity{" +
-				"id=" + id +
+				"id='" + id + '\'' +
+				", record_type=" + recordType +
 				", associated_organizations=" + associatedOrganizations +
 				", associated_people=" + associatedPeople +
+				", created_at=" + createdAt +
+				", modified_at=" + modifiedAt +
 				'}';
 	}
 }
--- src/main/java/com/vernalfinancial/fakebackendservice/models/VFRecordType.java
+++ src/main/java/com/vernalfinancial/fakebackendservice/models/VFRecordType.java
@@ -22,5 +22,6 @@ public enum VFRecordType {
 	Person,
 	Organization,
 	PersonalName,
-	OrganizationalName
+	OrganizationalName,
+	LoginCredentials
 }
