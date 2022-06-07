0b66f15e00481e774b9878661495bfd4063ca6cb
Matthew Crowell <78377986+Cahlien@users.noreply.github.com>
2022-06-07 18:47:04 -0500

    add spring data rest configuration

---
+++ src/main/java/com/vernalfinancial/fakebackendservice/config/RestConfiguration.java
@@ -0,0 +1,17 @@
+package com.vernalfinancial.fakebackendservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.ExposureConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+//		ExposureConfiguration exposureConfiguration =
+	}
+}
